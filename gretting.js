a73847bae2c9b3a4869da78d33f81d73f8e8efe6
richheadco <richheadco@users.noreply.github.com>
2020-03-31 23:34:57 +0900

    2020-03-31
    next is delete some list

--- gretting.js
+++ gretting.js
@@ -5,16 +5,33 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentVlaue = input.value;
+    paintGreeting(currentVlaue);
+    saveName(currentVlaue);
+}
+
+function askForNmae() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`
+    greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
         // 유저가 없는 경우!
+        askForNmae();
     } else {
         // 유저가 있는 경우!
         paintGreeting(currentUser);
