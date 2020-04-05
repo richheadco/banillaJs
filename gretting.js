a583e1d83400c99c7e81e2cb45c376a3e33f130a
richheadco <richheadco@users.noreply.github.com>
2020-04-05 18:45:33 +0900

    js complete
    last css control need

--- gretting.js
+++ gretting.js
@@ -24,7 +24,7 @@ function askForNmae() {
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `Hello, ${text} !! What are you doing today?`;
 }
 
 function loadName() {
